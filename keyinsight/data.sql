8136fc0537a17a09e0cbdc0a6a3c8e678234f0f2
robokae <alexanderhom19@gmail.com>
2022-03-10 11:51:02 -0800

    add database SQL script

---
+++ keyinsight/data.sql
@@ -0,0 +1,30 @@
+---- ======================================================
+---- Database Script
+---- ======================================================
+
+-- Create tables
+create table Users (
+    id integer primary key,
+    username varchar(128) unique not null,
+    password varchar(64) not null,
+    email varchar(128) unique not null,
+    team varchar(128) not null
+);
+
+create table Projects (
+    id integer primary key
+    -- insert remaining schema here
+);
+
+create table Servers (
+    id integer primary key
+    -- insert remaining schema here
+);
+
+-- Insert data into tables
+insert into Users values ('John123', 'u43oqo43quotjasd', 'john123@wd.com', 'backend');
+
+-- Queries
+select username, email
+from Users
+where team = 'backend';
