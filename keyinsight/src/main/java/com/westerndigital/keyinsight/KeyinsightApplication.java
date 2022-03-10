87581b6d797dfb8f2c57a8b99f6fb85b298c6cb5
robokae <alexanderhom19@gmail.com>
2022-03-10 10:50:24 -0800

    create Spring files and update gitignore

---
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/KeyinsightApplication.java
@@ -0,0 +1,13 @@
+package com.westerndigital.keyinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyinsightApplication.class, args);
+	}
+
+}
---
+++ keyinsight/src/test/java/com/westerndigital/keyinsight/KeyinsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westerndigital.keyinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyinsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
