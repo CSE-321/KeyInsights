cbfaf6697bff6d620b6c478eba4ea23d0ca61b5b
Alex Roman <aroman27@ucmerced.edu>
2022-03-15 04:17:36 -0700

    Included  Navbar at top of app for all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 
+import NavBar from "./Components/NavBar";
 import HomePage from './Views/HomePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        
+        <NavBar isUserSignedIn={false}/>
       </div>
 
       <div className="PageBody">
