aebb23fcf1a8c929eea791d584dbe9544f821373
Alex Roman <aroman27@ucmerced.edu>
2022-03-15 01:00:33 -0700

    Created a HomePage component

---
+++ frontend/src/Views/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+
+    return(
+        <>
+            <div>
+            <h1> Hello world </h1>
+            <p1> Here is the home page</p1>
+            </div>
+            
+        </>
+    );
+    
+    
+};
+
+export default HomePage;
