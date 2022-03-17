d21a8d67142ad234b56e720f87cc4d989dff0ee8
Alex Roman <aroman27@ucmerced.edu>
2022-03-17 11:44:35 -0700

    Added body header to home page

--- frontend/src/Views/HomePage.js
+++ frontend/src/Views/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import BodyHeader from '../Components/BodyHeader';
 const HomePage = () => {
-
+    const styling = {
+        color: 'white'
+    }
     return(
        <>
-              <BodyHeader title={"Home"} subtext={"Welcome home, lets get you started"}/>
+              <BodyHeader title={"Home"} subtext={"Welcome home, lets get you started"} showServer={false}>
+                  <h1 style={styling}></h1>
+              </BodyHeader>
        </>
     );
     
