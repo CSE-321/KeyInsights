c61817ef6130632c803e56a340a60bffc88886b9
DaoistDuck <dragonoath123@gmail.com>
2022-03-17 13:11:27 -0700

    Found code to extract issue data

--- keyinsight/src/main/java/com/westerndigital/keyinsight/KeyinsightApplication.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/KeyinsightApplication.java
@@ -3,11 +3,51 @@ package com.westerndigital.keyinsight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+
+import java.io.IOException;
+import java.net.URI;
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class KeyinsightApplication {
+	private String username;
+	private String password;
+	private String jiraUrl;
+	private JiraRestClient restClient;
 
-	public static void main(String[] args) {
-		SpringApplication.run(KeyinsightApplication.class, args);
+	private KeyinsightApplication(String username, String password, String jiraUrl) {
+		this.username = username;
+		this.password = password;
+		this.jiraUrl = jiraUrl;
+		this.restClient = getJiraRestClient();
+	}
+
+	public static void main(String[] args) throws IOException {
+		// SpringApplication.run(KeyinsightApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
+		KeyinsightApplication myJiraClient = new KeyinsightApplication(dotenv.get("JIRA_USERNAME"),
+				dotenv.get("JIRA_PASSWORD"), dotenv.get("JIRA_URL"));
+		String issueKey = "B8X4-10277";
+		Issue issue = myJiraClient.getIssue(issueKey);
+		System.out.println(issue.getProject());
+
+		myJiraClient.restClient.close();
+	}
+
+	private Issue getIssue(String issueKey) {
+		return restClient.getIssueClient().getIssue(issueKey).claim();
+	}
+
+	private JiraRestClient getJiraRestClient() {
+		return new AsynchronousJiraRestClientFactory()
+				.createWithBasicHttpAuthentication(getJiraUri(), this.username, this.password);
+	}
+
+	private URI getJiraUri() {
+		return URI.create(this.jiraUrl);
 	}
 
 }
