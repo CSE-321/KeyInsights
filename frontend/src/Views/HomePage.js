34e9be31f631e8c47d294eb9df8b6b7b78462583
Alex Roman <aroman27@ucmerced.edu>
2022-03-23 23:55:28 -0700

    Fixed spacing, added tailwind usage example

--- frontend/src/Views/HomePage.js
+++ frontend/src/Views/HomePage.js
@@ -1,18 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const HomePage = () => {
-
-    return(
-        <>
-            <div>
-            <h1> Hello world </h1>
-            <p1> Here is the home page</p1>
-            </div>
-            
-        </>
-    );
-    
-    
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl"> Hello world </h1>
+        <p1> Here is the home page</p1>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
