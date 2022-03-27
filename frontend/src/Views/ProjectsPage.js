c4b6f0d50beeb13af02088738146e32813705609
Alex Roman <aroman27@ucmerced.edu>
2022-03-26 20:50:36 -0700

    Added body header, and grid layout

--- frontend/src/Views/ProjectsPage.js
+++ frontend/src/Views/ProjectsPage.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import BodyHeader from '../Components/BodyHeader';
 
 const ProjectsPage = () => {
-  return <></>;
+  return (
+    <>
+      <BodyHeader
+        title="Projects In Server "
+        subtext="Access your available project insights"
+        showServer={false}
+      />
+      <div className="flex flex-col mx-7">
+        <p className="text-gray-600"> Home/Server/Projects</p>
+        <div className="grid grid-cols-3 grid-flow-row gap-5"></div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectsPage;
