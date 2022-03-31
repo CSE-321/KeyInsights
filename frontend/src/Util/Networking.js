ccb90f5cab9719b652807a6fc7ea58ddfc92c6c9
Alex Roman <aroman27@ucmerced.edu>
2022-03-31 12:49:48 -0700

    Added a networking js

--- frontend/src/Util/Networking.js
+++ frontend/src/Util/Networking.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { signinUser, signoutUser } from '../Components/Auth/userSlice';
@@ -14,7 +15,7 @@ export function signIn(email, password) {
   let mockPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(user);
-      //reject(new Error('Error 500 - Internal Server Error'));
+      // reject(new Error('Error 500 - Internal Server Error'));
       //reject(new Error('Error 401 - Unauthorized'));
     }, 1000);
   });
@@ -26,4 +27,28 @@ export function signOut() {
   const dispatch = useDispatch();
   //possibly make call to server if backend needs to store sessions
   dispatch(signoutUser({}));
+}
+
+export async function getAllProjects(serverURL, endpoint, email, password) {
+  var myHeaders = new Headers();
+  myHeaders.append('Authorization', 'Basic YXJvbWFuMjc6UmFjaGVhbDI=');
+  myHeaders.append(
+    'Cookie',
+    'JSESSIONID=C2B6A21E74F1F654EBC114D3638172A8; atlassian.xsrf.token=ANOT-3S39-KOCL-6XXI_0c83f0e402512894418d518343b7f0f05c2ec03f_lin',
+  );
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow',
+  };
+
+  fetch('http://jira.cloud-stm.com:8080/rest/api/2/project/', {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'no-cors',
+  })
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
 }
