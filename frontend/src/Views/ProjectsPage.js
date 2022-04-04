71295df5a454298a4ec89e5713d1fe8eff3674eb
Alex Roman <aroman27@ucmerced.edu>
2022-04-04 12:40:11 -0700

    Project cards are now displayed from an array

--- frontend/src/Views/ProjectsPage.js
+++ frontend/src/Views/ProjectsPage.js
@@ -4,30 +4,29 @@ import { setActiveServer } from '../Components/Auth/serverSlice';
 import BodyHeader from '../Components/BodyHeader';
 import ProjectCard from '../Components/ProjectCard';
 import { getAllProjects } from '../Util/Networking';
-
-const project = {
-  id: `${Math.floor(Math.random() * 9999)}`,
-  name: 'BBX8',
-  type: 'Software',
-  lead: 'Jane doe',
-  image:
-    'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-  created: '2020-01-01',
-  issues: '999999999999999999999999999999999999999999999',
-  server: 'cloud-stm',
-};
+import { Project } from '../Util/Project';
 
 const ProjectsPage = () => {
   const activeServer = useSelector((state) => state.server.activeServer);
   const [projects, setProjects] = useState([]);
   console.log(activeServer);
+  let arrayOfProjects = [];
 
-  getAllProjects(
-    'http://jira.cloud-stm.com:8080',
-    '/rest/api/latest/project',
-    'aroman27',
-    'Racheal2',
-  );
+  for (let i = 0; i < 10; i++) {
+    const project = {
+      id: Math.floor(Math.random() * 9999),
+      name: `BBX${i}`,
+      type: 'Software',
+      lead: 'Jane doe',
+      image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+      created: '2020-01-01',
+      issues: Math.floor(Math.random() * 1000),
+      serever: 'cloud-stm',
+    };
+
+    arrayOfProjects.push(project);
+  }
   const headerTitle = activeServer
     ? 'Projects in ' + activeServer.name
     : 'Projects (no server selected)';
@@ -40,22 +39,10 @@ const ProjectsPage = () => {
       />
       <div className="flex flex-col mx-7 my-5">
         <p className="text-gray-600"> Home/Server/Projects</p>
-        <div className="flex flex-col sm:grid sm:grid-flow-row gap-5 sm:grid-cols-2 xl:grid-cols-3">
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
-          <ProjectCard project={project} />
+        <div className="flex flex-col sm:grid sm:grid-flow-row gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {arrayOfProjects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
         </div>
       </div>
     </>
