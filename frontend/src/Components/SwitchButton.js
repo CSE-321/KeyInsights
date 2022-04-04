f782709e70db478c1f60ef559a20c77240b0f367
Alex Roman <aroman27@ucmerced.edu>
2022-04-04 16:39:27 -0700

    Created a switch button

---
+++ frontend/src/Components/SwitchButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SwitchButton = () => {
+  return (
+    <>
+      <div id="switch-root" className="h-full w-full">
+        <div id="left-switch" className="h-full w-1/2 bg-gray-200">
+          help
+        </div>
+        <div id="right-switch" className="h-full w-1/2 bg-gray-200">
+          adsad
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SwitchButton;
