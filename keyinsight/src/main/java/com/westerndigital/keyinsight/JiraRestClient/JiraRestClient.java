8c28978921e243f7bf53a1e80ec057e2f1a0c5b7
DaoistDuck <dragonoath123@gmail.com>
2022-04-05 21:01:09 -0700

    Added Dependences for Jira Rest

---
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/JiraRestClient/JiraRestClient.java
@@ -0,0 +1,14 @@
+package com.westerndigital.keyinsight.JiraRestClient;
+
+public class JiraRestClient {
+
+    private String username;
+    private String password;
+    private String jiraUrl;
+    private JiraRestClient restClient;
+
+    public class JiraRestClient(String username, String password, String jiraUrl){
+        
+    }
+
+}
