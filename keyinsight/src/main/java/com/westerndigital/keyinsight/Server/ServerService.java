a4747cc998e1068aa873c0f39ebe4eeae4a7f83c
jrosales38 <jrosales38@ucmerced.edu>
2022-04-11 11:50:12 -0700

    Updated ServerService

--- keyinsight/src/main/java/com/westerndigital/keyinsight/Server/ServerService.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/Server/ServerService.java
@@ -2,8 +2,18 @@ package com.westerndigital.keyinsight.Server;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ServerService {
+    @Autowired
+    private ServerRepository serverRepository;
+
+    public ServerService(ServerRepository serverRepository){
+        this.serverRepository = serverRepository;
+    }
+
     public List<Server> getAllServers(){
+        
         return null;
     }
 }
