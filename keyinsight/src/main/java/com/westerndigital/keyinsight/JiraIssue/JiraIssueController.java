f480295ad9e7fb09eda0b712068df7550b633155
jrosales38 <jrosales38@ucmerced.edu>
2022-04-15 15:14:37 -0700

    Set up JiraIssue, JiraProject, and Noti.Setting Co

--- keyinsight/src/main/java/com/westerndigital/keyinsight/JiraIssue/JiraIssueController.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/JiraIssue/JiraIssueController.java
@@ -1,5 +1,38 @@
 package com.westerndigital.keyinsight.JiraIssue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.boot.orm.jpa.*;
+
+import java.rmi.ServerException;
+import java.util.List;
+
+@Controller
+
+@RequestMapping("api/v1/issue")
 
 public class JiraIssueController {
+@Autowired
+private JiraIssueService jiraIssueService;
+
+public JiraIssueController(JiraIssueService jiraIssueService) {
+    this.jiraIssueService = jiraIssueService;
+}
+
+@PostMapping(path = "/issues")
+public ResponseEntity<HttpStatus> addIssue(JiraIssue issue) {
+    if(addIssue(issue) == null) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+    else{
+        return ResponseEntity.ok(HttpStatus.CREATED);
+    }
+    
+}
 
 }
