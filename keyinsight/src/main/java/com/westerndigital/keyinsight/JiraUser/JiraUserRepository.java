ac8a6ff3ae33f558925a6a4dc2c1432e194378e3
robokae <alexanderhom19@gmail.com>
2022-04-22 17:28:04 -0700

    update user service, repository

--- keyinsight/src/main/java/com/westerndigital/keyinsight/JiraUser/JiraUserRepository.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/JiraUser/JiraUserRepository.java
@@ -3,4 +3,6 @@ package com.westerndigital.keyinsight.JiraUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JiraUserRepository extends JpaRepository<JiraUser, Long> {
+
+    JiraUser findByUsername(String username);
 }
--- keyinsight/src/main/java/com/westerndigital/keyinsight/JiraUser/JiraUserService.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/JiraUser/JiraUserService.java
@@ -1,19 +1,23 @@
 package com.westerndigital.keyinsight.JiraUser;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public class JiraUserService {
+public class JiraUserService implements UserDetailsService {
 
     @Autowired
     private JiraUserRepository jiraUserRepository;
 
+    @Override
+    public JiraUser loadUserByUsername(String username) {
+        return jiraUserRepository.findByUsername(username);
+    }
+    
     public void saveUser(JiraUser jiraUser) {
         jiraUserRepository.save(jiraUser);
     }
 
-    public long getCount() {
-        return jiraUserRepository.count();
-    }
+
 
 }
 
