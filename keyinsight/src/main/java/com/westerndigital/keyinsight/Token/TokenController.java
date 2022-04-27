774e6c4dd33c30d7d7d77fdadf9991abdf7cc583
robokae <alexanderhom19@gmail.com>
2022-04-26 19:55:27 -0700

    update token controller, service

--- keyinsight/src/main/java/com/westerndigital/keyinsight/Token/TokenController.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/Token/TokenController.java
@@ -1,10 +1,18 @@
 package com.westerndigital.keyinsight.Token;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.westerndigital.keyinsight.JiraUser.JiraUser;
+import com.westerndigital.keyinsight.JiraUser.JiraUserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +23,9 @@ public class TokenController {
 
     @Autowired
     public TokenService tokenService;
+
+    @Autowired
+    public JiraUserService jiraUserService;
     
     @GetMapping("/refresh")
     public void refreshToken(HttpServletRequest request, 
@@ -23,15 +34,40 @@ public class TokenController {
         final String AUTHORIZATION_HEADER_PREFIX = "Bearer ";
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
 
+        // check the header
         if (header == null || 
             header.startsWith(AUTHORIZATION_HEADER_PREFIX) == false) {
 
             // handle error
         }
 
-        String token = header.substring(AUTHORIZATION_HEADER_PREFIX.length());
-        String username = tokenService.getUsernameFromToken(token);
+        try {
+            // extract the refresh token from the header
+            String refresh_token = header
+                .substring(AUTHORIZATION_HEADER_PREFIX.length());
 
+            String username = tokenService.getUsernameFromToken(refresh_token);
+
+            // get the user data from the database
+            JiraUser user = jiraUserService.loadUserByUsername(username);
+
+            // generate a new access token
+            String requestUrl = request.getRequestURI();
+            String access_token = tokenService
+                .generateNewAccessToken(user, requestUrl);
+
+            // set the access token in the response
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+            Map<String, String> tokens = new HashMap<>();
+            tokens.put("access_token", access_token);
+            tokens.put("refresh_token", refresh_token);
+
+            new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+        } catch (Exception e) {
+            // handle exception
+        }
         
     }
 }
--- keyinsight/src/main/java/com/westerndigital/keyinsight/Token/TokenService.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/Token/TokenService.java
@@ -1,11 +1,14 @@
 package com.westerndigital.keyinsight.Token;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.westerndigital.keyinsight.JiraUser.JiraUser;
 
 import org.springframework.stereotype.Service;
 
@@ -20,13 +23,32 @@ public class TokenService {
     private JWTVerifier verifier = JWT.require(algorithm).build();
     private DecodedJWT decodedJWT;
     
-    public Map<String, String> generateNewToken() {
+    public String generateNewAccessToken(JiraUser user, String requestUrl) {
+        String username = user.getUsername();
+        List<String> roles = new ArrayList<>();
 
-        return null;
+        final int ACCESS_TOKEN_EXPIRATION_MILLIS = 30 * 60 * 1000; 
+
+        user.getAuthorities().stream()
+            .forEach(authority -> {
+                roles.add(authority.getAuthority());
+            });
+
+        String access_token = JWT.create()
+            .withSubject(username)
+            .withIssuer(requestUrl)
+            .withClaim("role", roles)
+            .withExpiresAt(new Date(System.currentTimeMillis() + 
+                ACCESS_TOKEN_EXPIRATION_MILLIS))
+            .sign(algorithm);
+
+        return access_token;
+
     }
 
     public String getUsernameFromToken(String token) {
         decodedJWT = verifier.verify(token);
         return decodedJWT.getSubject();
     }
+
 }
