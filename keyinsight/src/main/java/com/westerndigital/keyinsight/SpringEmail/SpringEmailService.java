6b6e30d2e7e197cef47f9bb219816bc4ce6b132b
DaoistDuck <dragonoath123@gmail.com>
2022-04-26 20:01:47 -0700

    removed broken email files from database

--- keyinsight/src/main/java/com/westerndigital/keyinsight/SpringEmail/SpringEmailService.java
+++
@@ -1,6 +0,0 @@
-package com.westerndigital.keyinsight.SpringEmail;
-
-public interface SpringEmailService {
-    void sendSimpleMessage(String to, String subject, String text);
-    void sendMessageWithAttatchment(String to, String subject, String text, String pathToAttatchment);
-}
