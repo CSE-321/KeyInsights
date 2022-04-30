ce0813fb396590fcc7f1599105c46ffb323e78b7
DaoistDuck <jim.nguyen2017@gmail.com>
2022-04-30 16:50:55 -0700

    Update KPI2Service.java

--- keyinsight/src/main/java/com/westerndigital/keyinsight/KPI2/KPI2Service.java
+++ keyinsight/src/main/java/com/westerndigital/keyinsight/KPI2/KPI2Service.java
@@ -1,7 +1,6 @@
 package com.westerndigital.keyinsight.KPI2;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.westerndigital.keyinsight.JiraIssue.JiraIssueRepository;
@@ -25,36 +24,30 @@ public class KPI2Service {
         System.out.print(teamTypes);
 
         KPI2 daysToCompleteIssueKPI2 = new KPI2();
-        ArrayList<Integer> daysNeedToCompleteTeamType = issueRepository.daysNeededToCompleteTotalJiraIssues(projectName);
+        ArrayList<Integer> daysNeedToCompleteTotal = issueRepository.daysNeededToCompleteTotalJiraIssues(projectName);
 
         double median = -1.0;
-        int size = daysNeedToCompleteTeamType.size();
+        int size = daysNeedToCompleteTotal.size();
         int middle = size / 2;
         if(size % 2 == 0){
-            median = (double)(daysNeedToCompleteTeamType.get(middle - 1) + daysNeedToCompleteTeamType.get(middle))/2;
+            median = (double)(daysNeedToCompleteTotal.get(middle - 1) + daysNeedToCompleteTotal.get(middle))/2;
         }else{
-            median = daysNeedToCompleteTeamType.get(middle);
+            median = daysNeedToCompleteTotal.get(middle);
         }
 
         System.out.println("Median day needed for this team is " + median);
 
         daysToCompleteIssueKPI2.setTeamType("All Jira Issues");
-        daysToCompleteIssueKPI2.setAverageDayToCompleteIssue(daysNeedToCompleteTeamType.stream().mapToInt(val -> val).average().orElse(0.0));
-        daysToCompleteIssueKPI2.setMaximumDayToCompleteIssues(daysNeedToCompleteTeamType.stream().mapToInt(val -> val).max().orElse(0));
-        daysToCompleteIssueKPI2.setMinimumDayToCompleteIssues(daysNeedToCompleteTeamType.stream().mapToInt(val -> val).min().orElse(0));
+        daysToCompleteIssueKPI2.setAverageDayToCompleteIssue(daysNeedToCompleteTotal.stream().mapToInt(val -> val).average().orElse(0.0));
+        daysToCompleteIssueKPI2.setMaximumDayToCompleteIssues(daysNeedToCompleteTotal.stream().mapToInt(val -> val).max().orElse(0));
+        daysToCompleteIssueKPI2.setMinimumDayToCompleteIssues(daysNeedToCompleteTotal.stream().mapToInt(val -> val).min().orElse(0));
         daysToCompleteIssueKPI2.setMedianDayToCompleteIssues(median);
         
         listofKPI2.add(daysToCompleteIssueKPI2);
 
         for(String teamType : teamTypes){
             daysToCompleteIssueKPI2 = new KPI2();
-            daysNeedToCompleteTeamType = issueRepository.daysNeededToCompleteTeamTypeJiraIssues(projectName, teamType);
-            System.out.println("Maximum day needed for this team is " + daysNeedToCompleteTeamType.get(0));
-            System.out.println("Maximum day needed for this team is " + Collections.max(daysNeedToCompleteTeamType));
-            System.out.println("Maximum day needed for this team is " + daysNeedToCompleteTeamType.stream().mapToInt(val -> val).max().orElse(-1));
-            System.out.println("Minimum day needed for this team is " + Collections.min(daysNeedToCompleteTeamType));
-            System.out.println("Minimum day needed for this team is " + daysNeedToCompleteTeamType.stream().mapToInt(val -> val).min().orElse(-1));
-            System.out.println("Average day needed for this team is " + daysNeedToCompleteTeamType.stream().mapToInt(val -> val).average().orElse(0.0));
+            ArrayList<Integer> daysNeedToCompleteTeamType = issueRepository.daysNeededToCompleteTeamTypeJiraIssues(projectName, teamType);
             median = -1.0;
             size = daysNeedToCompleteTeamType.size();
             middle = size / 2;
