d705370649d25e4dfe9169b3a02d65c227e3be75
Alex Roman <aroman27@ucmerced.edu>
2022-05-02 19:26:52 -0700

    Conformed to the new kpi endpoint

--- frontend/src/Features/ProjectInsights/KPIService.js
+++ frontend/src/Features/ProjectInsights/KPIService.js
@@ -1,5 +1,5 @@
-import { round } from './MathUtil';
-export function getKPI_1() {
+import { round, getProjectNameFromUrl } from './MathUtil';
+export function getKPI_1(projectName) {
   var myHeaders = new Headers();
   myHeaders.append('Content-Type', 'application/json');
 
@@ -9,33 +9,36 @@ export function getKPI_1() {
     redirect: 'follow',
     mode: 'no-cors',
   };
+  console.log(getProjectNameFromUrl(projectName));
 
-  return fetch('/api/v1/KPI1', requestOptions)
+  return fetch(
+    `/api/v1/KPI1/${getProjectNameFromUrl(projectName)}`,
+    requestOptions,
+  )
     .then((response) => response.json())
     .catch((error) => console.log('error', error));
 }
 
 export function getPercentageJiraClosed(kpi1_List) {
-  var totalJira = 0;
-  var totalJiraClosed = 0;
-
-  kpi1_List.forEach((kpi) => {
-    totalJira += kpi.totalJiraCount;
-    totalJiraClosed += kpi.closedJiraCount;
-  });
-
+  if (kpi1_List.length <= 0) {
+    return 0;
+  }
+  var totalJira = kpi1_List[0].totalJiraCount;
+  var totalJiraClosed = kpi1_List[0].closedJiraCount;
+  console.log(totalJira);
+  console.log(totalJiraClosed);
   return round((totalJiraClosed / totalJira) * 100, 2);
 }
 
 export function getTopTeamsByJiraClosed(kpi1_List) {
   var teamList = [];
-
-  kpi1_List.forEach((kpi) => {
+  for (var i = 1; i < kpi1_List.length; i++) {
+    var kpi = kpi1_List[i];
     teamList.push({
       team: kpi.teamType,
       closedJiraCount: kpi.closedJiraCount,
     });
-  });
+  }
 
   teamList.sort((a, b) => {
     return b.closedJiraCount - a.closedJiraCount;
@@ -68,13 +71,24 @@ export function convertToKeys(topTeamsList) {
 }
 
 export function getTopJiraClosedTeam(kpi1_List) {
-  var totalJira = 0;
-  var totalJiraClosed = 0;
-
-  kpi1_List.forEach((kpi) => {
-    totalJira += kpi.totalJiraCount;
-    totalJiraClosed += kpi.closedJiraCount;
-  });
+  if (kpi1_List.length <= 0) {
+    return [
+      {
+        id: 'Total',
+        label: 'Total Issues',
+        value: 0,
+        color: 'hsl(46, 70%, 50%)',
+      },
+      {
+        id: 'Closed',
+        label: 'Closed Issues',
+        value: 0,
+        color: 'hsl(56, 70%, 50%)',
+      },
+    ];
+  }
+  var totalJira = kpi1_List[0].totalJiraCount;
+  var totalJiraClosed = kpi1_List[0].closedJiraCount;
 
   return [
     {
@@ -94,7 +108,7 @@ export function getTopJiraClosedTeam(kpi1_List) {
 
 export function getCriticalNotCompleted(KPI1_List) {
   var criticalNotCompleted = [];
-
+  // KPI1_List.splice(0, 1);
   KPI1_List.forEach((kpi) => {
     criticalNotCompleted.push({
       team: kpi.teamType,
